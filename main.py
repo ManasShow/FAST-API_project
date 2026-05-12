550cb37f76015929ce3c666fb808c95217893f13
ManasShow <manasshow5@gmail.com>
2026-05-12 19:14:37 +0530

    add path parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Path
 import json
 app = FastAPI()
 
@@ -16,3 +16,12 @@ def hello():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient_id/{patient_id}')
+def view_patient(patient_id: str = Path(...,description='ID of the patient in DB',example='p001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail='patient not found')

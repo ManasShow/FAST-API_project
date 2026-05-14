7b209ed32f9aacef2a99d73a54b4f7de9e0f6187
ManasShow <manasshow5@gmail.com>
2026-05-14 21:24:22 +0530

    add query parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException,Path
+from fastapi import FastAPI,HTTPException,Path,Query
 import json
 app = FastAPI()
 
@@ -18,10 +18,30 @@ def view():
     return data
 
 @app.get('/patient_id/{patient_id}')
-def view_patient(patient_id: str = Path(...,description='ID of the patient in DB',example='p001')):
+def view_patient(patient_id: str = Path(...,description='ID of the patient in DB',examples='p001')):
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
     else:
         raise HTTPException(status_code=404, detail='patient not found')
+    
+@app.get('/sort')
+def sort_patients(sort_by : str = Query(...,description="chose height ,weight or bmi"), 
+                  order : str = Query("asc",description="sort in asc or dsc")
+                  ):
+    
+    valid_fields = ["height","weight","bmi"]
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400)
+    
+    if order not in ['asc','dsc']:
+        raise HTTPException(status_code=400)
+    
+    data = load_data()
+
+    sort_order = True if order == 'dsc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
+    return sorted_data
